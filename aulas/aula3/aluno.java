cec7d9d52c26ef0d82e558bfaf4492b7ba14912f
eduardobutzen <eduardo.butzen@ufn.edu.br>
2026-03-10 08:55:54 -0300

    aula3

---
+++ aulas/aula3/aluno.java
@@ -0,0 +1,9 @@
+package aula3;
+
+public class aluno {
+    
+    String nome =  "Eduardo";
+    String matricula = "102030";
+    String cpf = "02538558026";
+    
+}
--- aulas/aula3/aula3.java
+++ aulas/aula3/aula3.java
@@ -1,12 +1,34 @@
-package aula3;
+    package aula3;
+    import java.util.Scanner;
 
-public class aula3 {
+    public class aula3 {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+
+            Scanner teclado = new Scanner(System.in);
+            
+            int n;
+            aluno[] a = new aluno[3];
+            
+
+            for (n = 0; n < 3; n++) {
+
+
+                a[n] = new aluno();
+                System.out.println("Digite o nome, matricula e cpf do aluno " + (n+1) + ":");
+                
+                a[n].nome = teclado.nextLine();
+                a[n].matricula = teclado.nextLine();
+                a[n].cpf = teclado.nextLine();
+
+                System.out.println("aluno instaciado");
+                System.out.println("nome: " + a[n].nome);
+                System.out.println("matricula: " + a[n].matricula);
+                System.out.println("cpf: " + a[n].cpf);
+
+                teclado.close();
+            }
+
+        }
         
-        System.out.println("Teste github");
-
-
     }
-    
-}
